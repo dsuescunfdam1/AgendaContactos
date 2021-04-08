a10b4775d068b87cc6cac15769779f06fbc8cccb
amauleonldam1 <amauleonldam1@gmail.com>
2021-04-08 19:31:58 +0200

    Hecho Constructor y Capitalizar

--- src/ut7/Agenda/modelo/Profesional.java
+++ src/ut7/Agenda/modelo/Profesional.java
@@ -1,9 +1,40 @@
 package ut7.Agenda.modelo;
 
-public class Profesional {
+public class Profesional extends Contacto {
+	private String nombreEmpresa;
 
-	public Profesional() {
+	
 
+	public Profesional(String nombreEmpresa, String nombre, String apellidos, String telefono, String email) {
+		super(nombre, apellidos, telefono, email);
+		this.nombreEmpresa = capitalizar(nombreEmpresa);
+	
+	}
+
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+	
+	private String capitalizar (String nombreEmpresa){
+		 
+		if (nombreEmpresa.length() == 0 ) {
+			return nombreEmpresa;
+		}
+		return nombreEmpresa.substring(0, 1).toUpperCase() +
+				nombreEmpresa.substring(1).toLowerCase();
+	 
+	}
+	
+
+	
+	public String getFirmaEmail() {
+		String[] Firmas = new String["Atentamente", "Saludos"];
+		
+		return null;
 	}
 
 }
