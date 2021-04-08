60cdc188339b9087cfe7673becea18fa77d8b4f8
amauleonldam1 <amauleonldam1@gmail.com>
2021-04-08 20:37:33 +0200

    Fecha de nacimiento y relacion añadida. Firma, cumpleaños y toAS añadidos

--- src/ut7/Agenda/modelo/Personal.java
+++ src/ut7/Agenda/modelo/Personal.java
@@ -1,9 +1,54 @@
 package ut7.Agenda.modelo;
 
-public class Personal {
+import java.time.LocalDate;
 
-	public Personal() {
 
+public class Personal extends Contacto {
+	private LocalDate fechaNacimiento;
+	private Relacion parentesco;
+
+	public Personal(Relacion parentesco, LocalDate fechaNacimiento, String nombre, String apellidos, String telefono, String email) {
+		super(nombre, apellidos, telefono, email);
+		this.fechaNacimiento = fechaNacimiento;
+		this.parentesco = parentesco;
 	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	
+	public Relacion getParentesco() {
+		return parentesco;
+	}
+
+	public void setParentesco(Relacion parentesco) {
+		this.parentesco = parentesco;
+	}
+
+	public String getFirmaEmail() {
+		
+		String firma = "Un abrazo!!";
+		return firma;
+	}
+	
+	public boolean esCumpleaños() {
+		
+		return fechaNacimiento.getDayOfMonth() == LocalDate.now().getDayOfMonth() && fechaNacimiento.getMonthValue() == LocalDate.now().getMonthValue();
+		
+	}
+
+
+	public String toString() {
+		return super.toString() + "Fecha de Nacimiento: " + this.getFechaNacimiento() + "\n" + "Relación: " + this.getParentesco() + "\n" + "\n";
+	}
+	
+	
+
+	
 
 }
