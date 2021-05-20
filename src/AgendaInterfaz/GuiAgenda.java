88e687562ad7d42b6a5c671f93660ecf3d90780a
dsuescunfdam1 <dsuescunfdam1@gmail.com>
2021-05-20 21:04:26 +0200

    Terminado diseño de la interfaz

--- src/AgendaInterfaz/GuiAgenda.java
+++ src/AgendaInterfaz/GuiAgenda.java
@@ -10,6 +10,8 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
@@ -79,6 +81,53 @@ public class GuiAgenda extends Application {
 	private VBox crearPanelBotones() {
 		// a completar
 		VBox panel = new VBox();
+		panel.setPadding(new Insets(10));
+		areaTexto.setPromptText("Buscar");
+		areaTexto.setMinHeight(40);
+		areaTexto.setPadding(new Insets(40));
+		
+		rbtListarSoloNumero = new RadioButton("Listar Solo Numero");
+		
+		rbtListarTodo = new RadioButton("Listar Todo");
+		rbtListarTodo.setSelected(true);
+		
+		ToggleGroup grupo = new ToggleGroup();
+		rbtListarSoloNumero.setToggleGroup(grupo);
+		rbtListarTodo.setToggleGroup(grupo);
+		
+		btnListar = new Button("Listar");
+		btnListar.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		btnListar.setPrefWidth(250);
+		btnListar.setPadding(new Insets(40));
+		btnListar.setOnAction(event -> listar());
+		
+		
+		btnPersonalesEnLetra = new Button("Personales en Letra");
+		btnPersonalesEnLetra.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		btnPersonalesEnLetra.setPrefWidth(250);
+		btnPersonalesEnLetra.setOnAction(event -> contactosEnLetra('a'));
+		
+		btnPersonalesOrdenadosPorFecha = new Button("Contactos personales ordenados por fecha");
+		btnPersonalesOrdenadosPorFecha.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		btnPersonalesOrdenadosPorFecha.setPrefWidth(250);
+		btnPersonalesOrdenadosPorFecha.setOnAction(event -> personalesOrdenadosPorFecha());
+		
+		btnClear = new Button("Clear");
+		btnClear.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		btnClear.setPrefWidth(250);
+		btnClear.setOnAction(event -> clear());
+		btnClear.setPadding(new Insets(40));
+		
+		btnSalir = new Button("Clear");
+		btnSalir.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		btnSalir.setPrefWidth(250);
+		btnSalir.setOnAction(event -> salir());
+		
+		panel.getChildren().addAll(areaTexto, rbtListarTodo, rbtListarSoloNumero, btnListar, btnPersonalesEnLetra, btnPersonalesOrdenadosPorFecha, btnClear, btnSalir);
+		
+		
+		
+		
 
 		return panel;
 	}
@@ -131,37 +180,51 @@ public class GuiAgenda extends Application {
 		barra.getMenus().add(menu);
 
 		itemImportar = new MenuItem("Importar");
+		itemImportar.setAccelerator(
+				KeyCombination.keyCombination("Ctrl+I"));
 		itemImportar.setOnAction(event -> importarAgenda());
+		
 		itemExportarPersonales = new MenuItem("Exportar_Personales");
+		itemExportarPersonales.setAccelerator(
+				KeyCombination.keyCombination("Ctrl+E"));
 		itemExportarPersonales.setOnAction(event -> exportarPersonales());
 		
 		itemSalir = new MenuItem("Salir");
+		itemSalir.setAccelerator(
+				KeyCombination.keyCombination("Ctrl + S"));
 		itemSalir.setOnAction(event -> salir());
 		
-		menu.getItems().addAll(itemImportar, itemExportarPersonales);
+		menu.getItems().addAll(itemImportar, itemExportarPersonales, itemSalir);
 		
 		Menu menu2 = new Menu("Operaciones");
 		barra.getMenus().add(menu2);
 
 		itemBuscar = new MenuItem("Importar");
+		itemBuscar.setAccelerator(
+				KeyCombination.keyCombination("Ctrl+B"));
 		itemBuscar.setOnAction(event -> buscar());
+		
 		itemFelicitar = new MenuItem("Exportar_Personales");
+		itemFelicitar.setAccelerator(
+				KeyCombination.keyCombination("Ctrl+F"));
 		itemFelicitar.setOnAction(event -> felicitar());
 		
 		
 		
 		menu2.getItems().addAll(itemBuscar, itemFelicitar);
 		
-		Menu menu3 = new Menu("Operaciones");
+		Menu menu3 = new Menu("Help");
 		barra.getMenus().add(menu3);
 
 		itemAbout = new MenuItem("Importar");
+		itemAbout.setAccelerator(
+				KeyCombination.keyCombination("Ctrl+A"));
 		itemAbout.setOnAction(event -> about());
 		
 		
 		
 		
-		menu2.getItems().addAll(itemBuscar, itemFelicitar);
+		menu3.getItems().addAll(itemAbout);
 
 		return barra;
 	}
